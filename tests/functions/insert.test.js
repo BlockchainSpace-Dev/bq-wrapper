b067fc02fd6d218656194468305dc50ae050e697
softwaremakassar <softwaremakassar@gmail.com>
2022-09-15 12:02:46 +0800

    chore: fix jest issue esm modules

---
+++ tests/functions/insert.test.js
@@ -0,0 +1,6 @@
+import { insert } from "../../src/functions/index.js";
+import { BigQuery } from "@google-cloud/bigquery";
+
+describe("insert wrapper functions", () => {
+  it("should throw error when data not object or not have value", () => {});
+});
